0fc480b2860cc1199bfd8d520898e909d72c4496
Konstantin Pozharitskiy <tripless@mail.ru>
2022-09-30 18:56:29 +0300

    completed tasks of lesson02

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
+let title = "Glo первый проект JS",
+  screens = "Простые, Сложные, Интерактивные",
+  screenPrice = 1989,
+  rollback = 89,
+  fullPrice = 2590500,
+  adaptive = true;
 
-alert("Hello! This is home work number one!");
-
-console.log("Hello! This is console message");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(
+  `Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`
+);
+console.log(
+  `Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`
+);
+console.log(screens.toLowerCase().split(""));
+console.log(fullPrice * (rollback / 100));
